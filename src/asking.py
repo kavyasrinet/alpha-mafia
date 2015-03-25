d54f654e697bba3d12175e2a398debaebb2f660f
Evan Palmer <evansp01@gmail.com>
2015-03-25 01:53:33 -0400

    commented program so it's not super confusing

--- src/asking.py
+++ src/asking.py
@@ -11,6 +11,7 @@ import os.path
 import codecs
 from subprocess import Popen, PIPE
 
+#only look at sentences that have a chance
 phrases = ["is", "was", "are", "were"]
 def contains_phrase(sentence):
     for phrase in phrases:
@@ -18,7 +19,7 @@ def contains_phrase(sentence):
             return True
     return False
 
-
+#prevents from downloading from the internet every time. I have bad internet
 def cached_page(page_name):
     text = None
     page = page_name.replace(' ','').lower()
@@ -32,67 +33,49 @@ def cached_page(page_name):
 
 ny = cached_page('New York')
 
+#get sentences
 sentences = nltk.sent_tokenize(ny.strip())
-soi = [sentence for sentence in sentences if contains_phrase(sentence)]
-soi = sorted(soi, key=lambda x: len(x))
 
+#get sentences which could be questions
+sentences_of_interest = [sentence for sentence in sentences if contains_phrase(sentence)]
+#sort by length, so we look at the easier ones first
+sentences_of_interest = sorted(sentences_of_interest, key=lambda x: len(x))
+
+#complicated syntax for getting the stanford parses
 def stanford_parser():
     STANFORD_JARS_FOLDER = '../dependencies/stanford-parser'
     os.environ['STANFORD_PARSER'] = STANFORD_JARS_FOLDER
     os.environ['STANFORD_MODELS'] = STANFORD_JARS_FOLDER
     return stanford.StanfordParser()
 
+#could be a better pattern probably
 pattern = '__ < (NP $. (VP <, (VBZ|VBD|VBP|VB < is|are|was|were)))'
 
 parser = stanford_parser()
-for sentence in soi:
+for sentence in sentences_of_interest:
+    #parse the sentence
     p = parser.raw_parse_sents([sentence])[0]
+    #call tregex
     with open(os.devnull, 'wb') as devnull:
         tregex = Popen(['../dependencies/stanford-tregex/tregex.sh', pattern ,'-filter'], stdout=PIPE, stdin=PIPE, stderr=devnull)
+    #get the result
     out = tregex.communicate(str(p))
+    #no error checking #yolo
     try:
         tree = nltk.Tree.fromstring(out[0])
+        #find the start of the pattern
         while tree[0].label() != 'NP':
             tree = tree[1:]
         subj = ' '.join(tree[0].leaves())
+        #get rid of pronoun subjects a bit, should be done better
         if 'it' in subj.split() or 'It' in subj.split() or 'this' in subj.split() or 'This' in subj.split():
              continue
+        #build the sentence
         verbphrase = tree[1].leaves()
         verb = verbphrase[0]
         capverb = verb[0].upper()+verb[1:]
         phrase = verbphrase[1:]
         print capverb+' '+subj+' '+' '.join(phrase)+'?'
     except:
+        #who cares?
         pass
-    #print p.label()
-
-#sentences = parser.raw_parse_sents(("Hello, My name is Melroy.", "What is your name?"))
-# print sentences
-# # GUI
-# for sentence in sentences:
-#     sentence.draw()
-
-# ny = wikipedia.page('New York')
-# print ny.content
-
-# crux = wikipedia.page("Violin")
-# # print crux.title + "\n"
-# # print crux.url + "\n"
-# #print crux.content + "\n"  
-# content = crux.content
-
-# history =  crux.section("History")
-# summary = wikipedia.summary("Santiago", sentences=5)  
-#question = "Where is Crux located ?"
-#question = "What is it dominated by ?"
-#question = "Is it commonly known as the Southern Cross ?"
-#question = "Do the brightest stars of the Crux appear on the Austrailian flag ?"
-#question = "what is the most prominent dark nebula in the skies ?"
-#question = "Which constellation borders the Crux on east, north and west ?"
-#question = "Is the Crux easily visible from the Northern Hemisphere throughout the year ?"
-#question = "Is Santiago the capital of Chile ?"
-#question  = "Has the air pollution in Santiago reduced in 2010 ?"
-#question = "Are the eastern communes in Santiago richer than the western communes ?"
-#question = "What is the name of Santiago's public bus transport system ?"
-question = "Who made the instrument known as Lady Blunt ?"
-#text = nltk.sent_tokenize(content.split("\n"))
