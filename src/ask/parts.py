b7577fd24bfcca811a8584698d7b4e090f4d2b2d
kavyasrinet <kavyasrinet@Kavyas-Air.wv.cc.cmu.edu>
2015-04-03 12:14:00 -0400

    Added has been

--- src/ask/parts.py
+++ src/ask/parts.py
@@ -16,11 +16,45 @@ def question_parts(ranked, debug=False):
     for parts in pool.imap(question_part, ranked):
         if parts: yield parts
 
+def get_pattern(type):
+    #normal Is/Was/Were
+    if type ==1 :
+        pattern = '__ < (NP $. (VP <, (VBZ|VBD|VBP|VB <, is|are|was|were)))'
+    #variants of has been 
+    elif type ==2:
+        pattern = '__ < (NP $. (VP < (VP <,(VB|VBZ|VBP /VB*/ <, has|have) <2 (VP <, (VBN <, been))))) '
+    #has been if the previous fails
+    elif type == 3:
+        pattern = '__ < (NP $. (VP <,(VB|VBZ|VBP <, has|have) <2 (VP <, (VBN <, been)))) '
+    return pattern
+
+
+
 def question_part(sentence):
+    ls = ["has","been"]
+    ls2 = ["have","been"]  
+    #sentence = "Since the 1950s it has been regarded as a centre for intellectuals and artists."
     parse = parser.raw_parse_sents([sentence]).next().next()
     #call tregex
-    pattern = '__ < (NP $. (VP <, (VBZ|VBD|VBP|VB <, is|are|was|were)))'
+    #print sentence
+    type  =2
+    if type ==2: 
+        if all(word in sentence for word in ls) or all(word in sentence for word in ls2):
+            pattern = get_pattern(type)
+        else:
+            return
+    pattern = get_pattern(type)
+    #pattern = '__ < (NP $. (VP <, (VBZ|VBD|VBP|VB <, is|are|was|were)))'
     output = stanford.tregex(str(parse), pattern, [])
+    if not output[0] and type==2:
+        type =3
+        pattern = get_pattern(type)
+        output = stanford.tregex(str(parse), pattern, [])
+        if not output[0]:
+            print "Has been couldn't generate anything"
+            return
+
+
     #try to get the tree
     tree = None
     try:
@@ -39,6 +73,7 @@ def question_part(sentence):
     return (subj, verb, verb_object)
 
 if __name__ == '__main__':
+
     question = "HARDCODED QUESTION"
     print question_part(question)
     #unit testing
