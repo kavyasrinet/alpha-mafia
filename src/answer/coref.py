859919baaacdaa12e665d078d63d8644b9ddd9ab
Evan Palmer <evansp01@gmail.com>
2015-04-14 04:40:06 -0400

    indentation

--- src/answer/coref.py
+++ src/answer/coref.py
@@ -2,46 +2,40 @@ import xml.etree.ElementTree as ET
 import sys
 
 def coref_list(root):
-	coref_sets = {}
+    coref_sets = {}
+    for sen in root:
+        for item in sen:
+            if item.tag != 'coref':
+                continue
+            set_id = item.get('set-id')
+            if set_id not in coref_sets:
+                coref_sets[set_id] = (None,None)
+            #end if
 
-	for sen in root:
-		for item in sen:
-			if item.tag == 'coref':
-				set_id = item.get('set-id')
-				
-				if set_id not in coref_sets:
-					coref_sets[set_id] = (None,None)
-				#end if
+            curr_coref = []
+            for w in item:
+                curr_coref.append(w.text)
+            #end for
 
-				curr_coref = []
-				for w in item:
-					curr_coref.append(w.text)
-				#end for
-
-				if coref_sets[set_id][0] is None:
-					coref_sets[set_id] = (curr_coref, [curr_coref])
-				else:
-					coref_sets[set_id][1].append(curr_coref)
-				#end if
-			#end if
-		#end for
-	#end for
-
-	return coref_sets
+            if coref_sets[set_id][0] is None:
+                coref_sets[set_id] = (curr_coref, [curr_coref])
+            else:
+                coref_sets[set_id][1].append(curr_coref)
+            #end if
+        #end for
+    #end for
+    return coref_sets
 #end def
 
 #unit test
 if __name__ == '__main__':
-	if(len(sys.argv) != 2):
-		sys.stderr.write('Correct Usage: python coref.py <inputFilePath>\n')
-		sys.exit(1)
-	#end if
+    if(len(sys.argv) != 2):
+        sys.stderr.write('Correct Usage: python coref.py <inputFilePath>\n')
+        sys.exit(1)
+        #end if
 
-	#f = open(sys.argv[1], 'r')
-	#xml_text = f.read()
+    tree = ET.parse(sys.argv[1])
+    root = tree.getroot()
 
-	tree = ET.parse(sys.argv[1])
-	root = tree.getroot()
-
-	print coref_list(root)
+    print coref_list(root)
 #end if
